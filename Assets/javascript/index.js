edb609c96f09d674149be8e7fa2dae65d9843332
karugo-murage <murage.gidion@moringaschool.com>
2022-06-06 03:09:28 +0300

    add js file

--- Assets/javascript/index.js
+++ Assets/javascript/index.js
@@ -1,3 +1,40 @@
 function thankYou(){
     alert("Thankyou for contacting Me.")
 }
+const para = document.createElement("p");
+const node = document.createTextNode("Am a software Engineer, I develop websites.I can provide");
+para.appendChild(node);
+const element = document.getElementById("div1");
+element.appendChild(para);
+
+const para1= document.createElement("p");
+const skill = document.createTextNode("clan code. I also make the website more and more");
+para.appendChild(node);
+const tech = document.getElementById("div1");
+element.appendChild(para);
+
+const para2= document.createElement("p");
+const skill1 = document.createTextNode("clan code. I also make the website more and more");
+para.appendChild(node);
+const tech1 = document.getElementById("div1");
+element.appendChild(para);
+
+const para3= document.createElement("p");
+const skill2 = document.createTextNode("clan code. I also make the website more and more");
+para.appendChild(node);
+const tech2 = document.getElementById("div1");
+element.appendChild(para);
+
+const para4= document.createElement("p");
+const skill3 = document.createTextNode("clan code. I also make the website more and more");
+para.appendChild(node);
+const tech3 = document.getElementById("div1");
+element.appendChild(para);
+
+const para5= document.createElement("p");
+const skill4 = document.createTextNode("clan code. I also make the website more and more");
+para.appendChild(node);
+const tech4 = document.getElementById("div1");
+element.appendChild(para);
+
+
